8cf9e86a39a627c7e49361bf87f2646a415aabb5
korrawee <korrawee.somyos@gmail.com>
2022-12-17 20:38:37 +0700

    Add account dto

---
+++ src/account/dto/UpdateAccount.dto.ts
@@ -0,0 +1,10 @@
+export class UpdateAccountDto {
+    username: string;
+    password: string;
+    fullname: string;
+    role: string;
+    telephone: string;
+    performance: number;
+    details: Object;
+    mng_id: string;
+}
---
+++ src/account/dto/findAccount.dto.ts
@@ -0,0 +1,11 @@
+export class FindAccountDto {
+    account_id: string;
+    username: string;
+    password: string;
+    fullname: string;
+    role: string;
+    telephone: string;
+    performance: number;
+    details: Object;
+    mng_id: string;
+}
