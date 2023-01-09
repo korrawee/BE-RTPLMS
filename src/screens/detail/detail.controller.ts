c34a4b28ba9f1308236ee784ea2ce6d0cfaf0ccc
korrawee <korrawee.somyos@gmail.com>
2023-01-09 16:52:16 +0700

    Add : getData

--- src/screens/detail/detail.controller.ts
+++ src/screens/detail/detail.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param } from '@nestjs/common';
+import { DetailService } from './detail.service';
 
 @Controller('detail')
-export class DetailController {}
+export class DetailController {
+    constructor(private readonly detailService: DetailService){}
+
+    @Get('/shift/:shift_code')
+    getData(@Param('shift_code') shiftCode: string){
+        return this.detailService.getData(shiftCode);
+    }
+}
