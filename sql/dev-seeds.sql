2b9911ed86e83888f5a39287982632519c82f68f
korrawee <korrawee.somyos@gmail.com>
2023-01-09 16:45:08 +0700

    Modify : getting ready to serve data to detail api

--- sql/dev-seeds.sql
+++ sql/dev-seeds.sql
@@ -1,19 +1,52 @@
 do $$
-declare
-add integer := 0;
+    declare
+    index integer := 0;
 
-begin
-while add <10 loop
-    INSERT INTO accounts VALUES (add,'user','1234','full-name','worker','0981',12.0,'{"data": "eiei"}', NULL);
-    add := add+1;
-    INSERT INTO shifts VALUES (add, '2022-12-27', '08:00:00', 500, 100, 20, 30, 15);
-end loop;
+    begin
+    while index <10 loop
+        INSERT INTO accounts VALUES (
+            index,
+            concat('user', cast(index AS text)),
+            '1234',
+            concat('full-name', cast(index AS text)),
+            'worker',
+            '0981',
+            12.0,
+            '{"data": "eiei"}', 
+            NULL
+        );
+        INSERT INTO shifts VALUES (
+            index, 
+            cast(now() as date), 
+            '08:00:00', 
+            500, 
+            100, 
+            20, 
+            30, 
+            15
+        );
+        index := index+1;
+    end loop;
 
-    INSERT INTO departments VALUES (1, 'ต้มไก่', 1);
-    INSERT INTO departments VALUES (2, 'ต้มไก่', 1);
-    INSERT INTO _controls VALUES (1, 1);
-    INSERT INTO _controls VALUES (2, 2);
+        INSERT INTO departments VALUES (1, 'ต้มไก่', 1);
+        INSERT INTO departments VALUES (2, 'ต้มไก่', 1);
+        INSERT INTO _controls VALUES (1, 1);
+        INSERT INTO _controls VALUES (2, 2);
 
-    INSERT INTO work_on VALUES(1, 2, 1, '08:00', NULL, NULL, '2022-12-27');
-    INSERT INTO work_on VALUES(2, 3, 2, '08:00', NULL, NULL, '2022-12-27');
+        INSERT INTO work_on VALUES(2, 1, '07:00', NULL, NULL, cast(now() as date));
+        INSERT INTO work_on VALUES(3, 1, '08:00', NULL, NULL, cast(now() as date));
+        INSERT INTO work_on VALUES(4, 1, '08:01', NULL, NULL, cast(now() - INTERVAL '-1 days' as date));
+        INSERT INTO work_on VALUES(5, 1, NULL, NULL, NULL, cast(now() - INTERVAL '-1 days' as date));
+
+        INSERT INTO work_on VALUES(6, 2, '07:45', NULL, NULL, cast(now() as date));
+        INSERT INTO work_on VALUES(7, 2, '08:00', NULL, NULL, cast(now() as date));
+        INSERT INTO work_on VALUES(8, 2, '08:01', NULL, NULL, cast(now() - INTERVAL '-1 days' as date));
+
+        INSERT INTO requests values(1, 3, cast(now() as date), 4, 'รอดำเนินการ', '1');
+        INSERT INTO requests values(1, 4, cast(now() as date), 4, 'รอดำเนินการ', '1');
+        INSERT INTO requests values(1, 5, cast(now() - INTERVAL '-1 days' as date), 4, 'รอดำเนินการ', '1');
+
+        INSERT INTO requests values(2, 6, cast(now() as date), 4, 'รอดำเนินการ', '1');
+        INSERT INTO requests values(2, 7, cast(now() as date), 4, 'รอดำเนินการ', '1');
+        INSERT INTO requests values(2, 8, cast(now() - INTERVAL '-1 days' as date), 4, 'รอดำเนินการ', '1');
 end$$;
