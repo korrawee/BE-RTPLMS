79bbbeba3134d60247ac74002987810a8b1eab3e
korrawee <korrawee.somyos@gmail.com>
2023-03-09 14:30:43 +0700

    build: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
-import { DashboardController } from './screens/dashboard/dashboard.controller';
-import { DashboardService } from './screens/dashboard/dashboard.service';
-import { DepartmentService } from './department/department.service';
-import { DepartmentController } from './department/department.controller';
 import { PostgresModule } from 'nest-postgres';
 import { RequestModule } from './relations/request/request.module';
 import { WorkOnModule } from './relations/work-on/work-on.module';
@@ -19,34 +15,49 @@ import { LogModule } from './log/log.module';
 import { LoginModule } from './screens/login/login.module';
 import { ControlModule } from './relations/control/control.module';
 import { KafkaModule } from './kafka/kafka.module';
+import { DepartmentModule } from './department/department.module';
+import { DashboardModule } from './screens/dashboard/dashboard.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: `.${process.env.NODE_ENV}.env`,
-    }), 
-    PostgresModule.forRootAsync({
-      imports: [ConfigModule],
-      
-      useFactory: async(configService: ConfigService) => {
-        console.log({connectionString: `postgresql://${configService.get('POSTGRES_USER')}:${configService.get('POSTGRES_PASSWORD')}@${configService.get('POSTGRES_HOST')}:${configService.get<number>('POSTGRES_PORT')}/${configService.get('POSTGRES_DB')}`,
-      })
-        return {connectionString: `postgresql://${configService.get('POSTGRES_USER')}:${configService.get('POSTGRES_PASSWORD')}@${configService.get('POSTGRES_HOST')}:${configService.get<number>('POSTGRES_PORT')}/${configService.get('POSTGRES_DB')}`,
-      }},
-      inject: [ConfigService]
-    }),
-    AccountModule,
-    WorkOnModule,  
-    RequestModule, 
-    DetailModule, 
-    ScheduleModule, 
-    OtRequestModule, 
-    ShiftModule, 
-    LogModule, 
-    LogScreenModule, LoginModule, ControlModule, KafkaModule,
-  ],
-  controllers: [AppController, DashboardController, DepartmentController],
-  providers: [AppService, DashboardService, DepartmentService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.${process.env.NODE_ENV}.env`,
+        }),
+        PostgresModule.forRootAsync({
+            imports: [ConfigModule],
+
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    connectionString: `postgresql://${configService.get(
+                        'POSTGRES_USER'
+                    )}:${configService.get(
+                        'POSTGRES_PASSWORD'
+                    )}@${configService.get(
+                        'POSTGRES_HOST'
+                    )}:${configService.get<number>(
+                        'POSTGRES_PORT'
+                    )}/${configService.get('POSTGRES_DB')}`,
+                };
+            },
+            inject: [ConfigService],
+        }),
+        AccountModule,
+        DashboardModule,
+        DepartmentModule,
+        WorkOnModule,
+        RequestModule,
+        DetailModule,
+        ScheduleModule,
+        OtRequestModule,
+        ShiftModule,
+        LogModule,
+        LogScreenModule,
+        LoginModule,
+        ControlModule,
+        KafkaModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
