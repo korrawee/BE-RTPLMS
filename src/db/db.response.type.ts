b96a70fe2743d3133e04523ea7cf5f6c7697850c
korrawee <korrawee.somyos@gmail.com>
2023-03-09 14:32:36 +0700

    build: add indentation

--- src/db/db.response.type.ts
+++ src/db/db.response.type.ts
@@ -1,10 +1,9 @@
 export class dbResponse {
-
-    "command": string;
-    "rowCount": number;
-    "oid": any;
-    "rows": Array<any>;
-    "_types": Object;
-    "RowCtor": any;
-    "rowAsArray": boolean;
+    'command': string;
+    'rowCount': number;
+    'oid': any;
+    'rows': Array<any>;
+    '_types': Object;
+    'RowCtor': any;
+    'rowAsArray': boolean;
 }
