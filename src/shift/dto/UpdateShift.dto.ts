44b293a588ec7e46a2afb3244c4bb68e8aeb4f10
korrawee <korrawee.somyos@gmail.com>
2023-03-21 04:06:54 +0700

    build: update dto

--- src/shift/dto/UpdateShift.dto.ts
+++ src/shift/dto/UpdateShift.dto.ts
@@ -3,8 +3,10 @@ export class UpdateShiftDto {
     date?: string;
     shift_time?: string;
     product_target?: string;
-    success_product?: number;
-    ideal_performance?: number;
+    success_product_in_shift_time?: string;
+    success_product_in_OT_time?: string;
+    ideal_performance?: string;
     all_member?: number;
     checkin_member?: number;
+    department_id?: string;
 }
