785326a87fe4d360ee5a9c890a283057d6f01bca
korrawee <korrawee.somyos@gmail.com>
2023-04-12 11:09:52 +0700

    build: add limit and current page params

--- src/screens/dashboard/dashboard.controller.ts
+++ src/screens/dashboard/dashboard.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 @Controller('dashboard')
 export class DashboardController {
     constructor(private readonly dashboardService: DashboardService) {}
 
     @Get('/:mng_id')
-    async getData(@Param('mng_id') mngId: string) {
-        const data = await this.dashboardService.getData(mngId);
+    async getData(@Param('mng_id') mngId: string, @Query('limit') limit: string, @Query('currrentPage') currentPage: number) {
+        const data = await this.dashboardService.getData(mngId, limit || '*', currentPage || 1);
         return data;
     }
 }
