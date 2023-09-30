34593add99b2c1b37b99731661d6a0f81402c4c9
kantinun <kantinun.sirintharawet@gmail.com>
2023-09-30 12:38:53 +0700

    add comment in product consumer

--- src/kafka/consumer/product.consumer.ts
+++ src/kafka/consumer/product.consumer.ts
@@ -44,20 +44,27 @@ export class ProductConsumer implements OnModuleInit {
         // Prep. data
         const value: { shift_code: string; success_product: number } =
             JSON.parse(message.value.toString());
+        //Get shift detail data
         const shift: ShiftDto = await this.shiftService.getShiftById(value.shift_code);
+
+        //////// Prepare update parameter model/////////
         delete shift.actual_performance;
         const updatedShift = { ...shift };
+        ///////////////////////////////////////////////
 
         // Update success_product
+        ///// Find max accepted ot request time because it the end time of this shift //////////
         const request_list = await this.requestService.getAllRequestByShift_id(value.shift_code)
         const shift_OT_time = request_list.filter((req)=>req.req_status==="ยอมรับ").length!=0?Math.max(...request_list.filter((req)=>req.req_status==="ยอมรับ").map((req)=>req.number_of_hour)):0
-        const shift_start_time = moment(`${moment(shift.date).format("YYYY-MM-DD")} ${shift.shift_time}`, "YYYY-MM-DD HH:mm:ss")
-        const shift_plan_end_time = moment(`${moment(shift.date).format("YYYY-MM-DD")} ${shift.shift_time}`, "YYYY-MM-DD HH:mm:ss").add(8,'hours')
-        const shift_OT_end_time = moment(`${moment(shift.date).format("YYYY-MM-DD")} ${shift.shift_time}`, "YYYY-MM-DD HH:mm:ss").add(8+shift_OT_time,'hours')
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+        const shift_start_time = moment(`${moment(shift.date).format("YYYY-MM-DD")} ${shift.shift_time}`, "YYYY-MM-DD HH:mm:ss") // Shift start time
+        const shift_plan_end_time = moment(`${moment(shift.date).format("YYYY-MM-DD")} ${shift.shift_time}`, "YYYY-MM-DD HH:mm:ss").add(8,'hours') // Shift end time (Exclude OT time)
+        const shift_OT_end_time = moment(`${moment(shift.date).format("YYYY-MM-DD")} ${shift.shift_time}`, "YYYY-MM-DD HH:mm:ss").add(8+shift_OT_time,'hours') // shift end time (Include OT time)
         
         //check shift started?
         if(moment().isAfter(shift_start_time) && moment().isBefore(shift_plan_end_time)){
-            //if started
+            //if started we will update variable that collect success product that happen in shift time exclude ot time
                 
             updatedShift.success_product_in_shift_time =
                 (
@@ -67,7 +74,7 @@ export class ProductConsumer implements OnModuleInit {
                 .toString();
         }else if(moment().isAfter(shift_plan_end_time) && moment().isBefore(shift_OT_end_time)){
             
-            //if on OT_time
+            //if on OT_time we will update variable that collect success product that happen in ot time
             updatedShift.success_product_in_OT_time =
                 (
                     (+updatedShift.success_product_in_OT_time) + 
@@ -80,7 +87,9 @@ export class ProductConsumer implements OnModuleInit {
         await this.shiftService.updateShift(updatedShift);
 
         // Notices to client
+         //Get department detail data
         const department: DepartmentforDashboardDto = await this.departmentService.getDepartmentById(updatedShift.department_id)
+        //Push message to websocket to trigger frontend side of department's manager to fetch new data
         if(this.sendNoticeToClient(department.mng_id)){
             console.log('Update consumed product successfully.');
         }else{
